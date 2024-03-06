4797e6082dd5d4a93a43edef1a4be25d25092aae
iftekher2108 <parvezkhan21082002@gmail.com>
2024-03-06 17:39:21 +0600

    want to ui change

---
+++ resources/views/components/admin_panel.blade.php
@@ -0,0 +1,21 @@
+<div class="panel-menu rounded bg-dark text-white p-3">
+    <ul class="fs-5">
+     <li class="nav-item">
+        <a href="" class="nav-link">Dashboard</a>
+    </li>
+    <li class="nav-item">
+        <a href="" class="nav-link"></a>
+    </li>
+    <li class="nav-item">
+        <a href="" class="nav-link"></a>
+    </li>
+    <li class="nav-item">
+        <a href="" class="nav-link"></a>
+    </li>
+    <li class="nav-item">
+        <a href="" class="nav-link">Settings</a>
+    </li>
+    </ul>
+
+
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-md-4">
+            <x-admin_panel/>
+        </div>
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
